eb6e257fe28c0c6bbe7d1780e50b25371b5dd2b0
MontesOscar <20233TN073@utez.edu.mx>
2024-05-23 18:35:22 -0600

    cambio nombre, try delete

--- hello_world/app.py
+++ hello_world/app.py
@@ -25,18 +25,12 @@ def lambda_handler(event, context):
         Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
     """
 
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
 
-    #     raise e
 
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "nombre-completo": "Kevin Yael Guzman Diaz",
+            "nombre-completo": "Yutzil Fuentes",
             "grado":"9",
             "grupo":"B"
             # "location": ip.text.replace("\n", "")
